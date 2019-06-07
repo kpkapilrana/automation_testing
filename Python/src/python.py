a0cd4c2176e6eb54700f5794220ca514b9944b22
Kapil Patil <kpkapilrana@gmail.com>
2019-06-07 19:40:48 +0530

    pyt

---
+++ Python/src/python.py
@@ -0,0 +1,18 @@
+'''
+Created on 20-Oct-2018
+
+@author: Legend's
+'''
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+user = ""
+pwd = ""
+driver = webdriver.Firefox()
+driver.get("http://www.facebook.com")
+assert "Facebook" in driver.title
+elem = driver.find_element_by_id("email")
+elem.send_keys(user)
+elem = driver.find_element_by_id("pass")
+elem.send_keys(pwd)
+elem.send_keys(Keys.RETURN)
+driver.close()
